1de82319489e20272b7ba15d44ff3975e21c0657
sunilsigmato <sunil@sigmato.com>
2024-04-19 17:15:26 +0530

    coupon

--- application/frontend/views/coupon/coupon.php
+++ application/frontend/views/coupon/coupon.php
@@ -462,7 +462,7 @@ if($get_breadcrumb)
                                                 <div class="d-flex align-items-center">
                                                     <p>
                                                         <i class="fa-solid fa-check verify-icon"></i>
-                                                        Verified today
+                                                        Verified Today
                                                     </p>
                                                     <p class="ps-5"> <i class="fa-solid fa-users"></i> 218 People Used
                                                         Today
@@ -504,74 +504,90 @@ if($get_breadcrumb)
 
                                             <style>
                                                 
-
-
-.option-input {
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  -ms-appearance: none;
-  -o-appearance: none;
-  appearance: none;
-  position: relative;
-  top: 13.33333px;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  height: 40px;
-  width: 40px;
-  transition: all 0.15s ease-out 0s;
-  background: #cbd1d8;
-  border: none;
-  color: #fff;
-  cursor: pointer;
-  display: inline-block;
-  margin-right: 0.5rem;
-  outline: none;
-  position: relative;
-  z-index: 1000;
-}
-.option-input:hover {
-  background: #9faab7;
-}
-.option-input:checked {
-  background: #40e0d0;
-}
-.option-input:checked::before {
-  width: 40px;
-  height: 40px;
-  display:flex;
-  content: '\f00c';
-  font-size: 25px;
-  font-weight:bold;
-  position: absolute;
-  align-items:center;
-  justify-content:center;
-  font-family:'Font Awesome 5 Free';
-}
-.option-input:checked::after {
-  -webkit-animation: click-wave 0.65s;
-  -moz-animation: click-wave 0.65s;
-  animation: click-wave 0.65s;
-  background: #40e0d0;
-  content: '';
-  display: block;
-  position: relative;
-  z-index: 100;
-}
-.option-input.radio {
-  border-radius: 50%;
-}
-.option-input.radio::after {
-  border-radius: 50%;
-}
+                                        .option-input {
+                                        -webkit-appearance: none;
+                                        -moz-appearance: none;
+                                        -ms-appearance: none;
+                                        -o-appearance: none;
+                                        appearance: none;
+                                        position: relative;
+                                        top: 13.33333px;
+                                        right: 0;
+                                        bottom: 0;
+                                        left: 0;
+                                        height: 40px;
+                                        width: 40px;
+                                        transition: all 0.15s ease-out 0s;
+                                        background: #cbd1d8;
+                                        border: none;
+                                        color: #fff;
+                                        cursor: pointer;
+                                        display: inline-block;
+                                        margin-right: 0.5rem;
+                                        outline: none;
+                                        position: relative;
+                                        z-index: 1000;
+                                        }
+                                        .option-input:hover {
+                                        background: #9faab7;
+                                        }
+                                        .option-input:checked {
+                                        background: #40e0d0;
+                                        }
+                                        .option-input:checked::before {
+                                        width: 40px;
+                                        height: 40px;
+                                        display:flex;
+                                        content: '\f00c';
+                                        font-size: 25px;
+                                        font-weight:bold;
+                                        position: absolute;
+                                        align-items:center;
+                                        justify-content:center;
+                                        font-family:'Font Awesome 5 Free';
+                                        }
+                                        .option-input:checked::after {
+                                        -webkit-animation: click-wave 0.65s;
+                                        -moz-animation: click-wave 0.65s;
+                                        animation: click-wave 0.65s;
+                                        background: #40e0d0;
+                                        content: '';
+                                        display: block;
+                                        position: relative;
+                                        z-index: 100;
+                                        }
+                                        .option-input.radio {
+                                        border-radius: 50%;
+                                        }
+                                        .option-input.radio::after {
+                                        border-radius: 50%;
+                                        }
 
                                                 </style>
 
 
+                                         <div class="align-items-center">
+                                            <label>
+                                            <input type="radio" value="today" class="option-input radio date_radio" name="group1" />
+                                                Today
+                                            </label>
+                                            <label>
+                                                <input type="radio" value="tommorow" class="option-input radio date_radio" name="group1" />
+                                                Tommorow
+                                            </label>
+                                            <label>
+                                                <input type="radio" value="day_after_tmr" class="option-input radio date_radio" name="group1" />
+                                                Day After Tomorrow
+                                            </label>
+                                    </div>
 
 
-
-                                            <center>
+                                            <?php }else{ ?>
+                                                <fieldset id="group1">
+                                                <a href="javascript:void(0)" class=" text-right btn-md mb-1"
+                                                data-bs-effect="effect-scale" data-bs-toggle="modal"
+                                                data-bs-target="#login-button">
+                                                <center>
                                             <label>
                                             <input type="radio" value="today" class="option-input radio date_radio" name="group1" />
                                             Today
@@ -584,17 +600,7 @@ if($get_breadcrumb)
                                             <input type="radio" value="day_after_tmr" class="option-input radio date_radio" name="group1" />
                                             Day After Tomorro
                                         </label>
-                                        </center>
-
-
-                                            <?php }else{ ?>
-                                                <fieldset id="group1">
-                                                <a href="javascript:void(0)" class=" text-right btn-md mb-1"
-                                                data-bs-effect="effect-scale" data-bs-toggle="modal"
-                                                data-bs-target="#login-button">
-                                                <input type="radio" value="today" name="group1" class="date_radio"> Today  
-                                                <input type="radio" value="tommorow" name="group1" class="date_radio"> Tommorow    
-                                                <input type="radio" value="day_after_tmr" name="group1" class="date_radio"> Day After Tomorrow   
+                                        </center> 
                                             </a>
                                             </fieldset>
                                             <?php } ?>
@@ -606,17 +612,21 @@ if($get_breadcrumb)
                                                 
                                                
                                             ?>
-                                            <table border='1'>
+                                            <table  class="table">
+                                            <thead>
                                                 <tr>
                                                       <td class = "today_header">Today</td>
                                                       <td class = "tmr_header">Tommorow </td>
                                                       <td class = "day_after_tmr_header">Day after Tommorow </td>
                                                 </tr>
-                                                <tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr class="danger">
                                                     <td class="today"><?php echo $res_coupon_count_today ?></td>
                                                     <td class="tommorow"><?php echo $res_coupon_count_tommorow ?></td>
                                                     <td class="day_after_tmr_data"><?php echo $res_coupon_count_tommorow ?></td>
                                                 </tr>
+                                            </tbody>
                                         </table>
                                         </div>
                                         <div class="d-flex align-items-center justify-content-center date_div">
@@ -627,6 +637,8 @@ if($get_breadcrumb)
                                                 style="width:300px ">
                                                 
                                         </div>
+
+                                        
                                         
                                         <hr>
                                        <!-- <div class="d-flex align-items-center justify-content-center">
